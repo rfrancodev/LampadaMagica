7c73ef9b43db3797225d49a5f2a3335a86787e4f
rfrancodev <francorfs@icloud.com>
2024-10-10 20:12:23 -0300

    Initial Commit

---
+++ LampadaMagica/LampadaMagicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LampadaMagicaApp.swift
+//  LampadaMagica
+//
+//  Created by Rafael Franco on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LampadaMagicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
